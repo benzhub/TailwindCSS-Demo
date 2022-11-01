92ed372281cef1e19406a1cf144d4c84b0da1479
benzhub <lenrich1403@gmail.com>
2022-11-01 14:14:19 +0800

    config 自定義主題

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+/* 叫出完整預設的tailwind.config.js => npx tailwindcss init tailwind-full.config.js --full */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   target: 'web',
   // tailwind打包的時候，會先去掃描所有的html去看哪些會使用到tailwind utilities，只會打包使用到的tailwind 
   content: ['./src/**/*.{html,js}'],
+  darkMode: false, // or 'media' or 'class'
+  // 在做擴展的時候，一定要放在extend裡面，不然會覆蓋原本的設定
   theme: {
     extend: {
       screens: {
         'xx': '1000px'
+      },
+      colors: {
+        'fill': '#fff'
       }
     }
   },
