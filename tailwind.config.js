e6b579ed3d2431ea76ac43575470c8b6b3bc9a0f
benzhub <lenrich1403@gmail.com>
2022-11-01 14:48:40 +0800

    JIT mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /* 叫出完整預設的tailwind.config.js => npx tailwindcss init tailwind-full.config.js --full */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // Just in Time 模式，加速你的dev開發編譯速度
+  mode: 'jit',
   target: 'web',
   // tailwind打包的時候，會先去掃描所有的html去看哪些會使用到tailwind utilities，只會打包使用到的tailwind 
   content: ['./src/**/*.{html,js}'],
