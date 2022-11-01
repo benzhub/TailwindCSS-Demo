c7782898ebe75cd09a7806b4ddd6bcd78dd628dc
benzhub <lenrich1403@gmail.com>
2022-11-01 15:12:20 +0800

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   target: 'web',
   // tailwind打包的時候，會先去掃描所有的html去看哪些會使用到tailwind utilities，只會打包使用到的tailwind 
   content: ['./src/**/*.{html,js}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class', media會用使用者的系統默認的明亮模式，class是讓使用者自己手動切換
   // 在做擴展的時候，一定要放在extend裡面，不然會覆蓋原本的設定
   theme: {
     extend: {
